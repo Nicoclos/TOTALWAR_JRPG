d35f91c31cda3c91b341a7c14dd455e48c08a779
Nicolas Bia <n.bia@outlook.fr>
2022-01-07 03:18:56 +0100

    I'm sleepy sensei... sorry I failed... gomen nasai...

--- game.js
+++ game.js
@@ -3,11 +3,12 @@ import Berzerker from "./berzerker.js";
 import Fighter from "./fighter.js";
 import Monk from "./monk.js";
 import Paladin from "./paladin.js";
+import Turn from "./turn.js";
 
 class Game {
   constructor (turnLeft = 10) {
     this.turnLeft = turnLeft;
-    this.characters = [
+    this.sentōki = [
       new Fighter(),
       new Paladin(),
       new Monk(),
@@ -15,7 +16,47 @@ class Game {
       new Assassin()
     ];
   }
+
+  newTurn = () => {
+    this.turnLeft -= 1;
+  }
+
+  gameProcess = () => {
+    if (this.turnLeft === 10) {
+      console.log("三、二、一、開始 !");
+    }
+
+    if (this.sentōki.hasStatus === "loser"){
+      this.sentōki = false;
+    }
+
+    if (this.turnLeft === 0) {
+      console.log(" DING DING DING ! THE FIGHT IS OVER");
+      if (this.sentōki.hp >= 1) {
+        hasStatus = "winner";
+        if (this.sentōki.hasStatus === "winner") {
+          console.log(`Congratulations to our winner(s) of the day ! ${this.sentōki.filter((won) => won.hasStatus === "winner")}`);
+        }
+        
+        anotherOne = () => {
+          let restart = prompt("Do you want to play once more ?")
+          if (restart == "Yes", "Let's go") {
+            new Game(this.turnLeft = 10)
+          }
+          
+          else {
+            console.log("You're just a sucker anyway ! Go away noob !")
+          }
+    
+        }
+  
+      }
+    }
+  }
 }
 
 let newGame = new Game();
 console.log(newGame);
+
+let newTurn = new Turn();
+console.log(newTurn);
